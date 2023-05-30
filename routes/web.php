ff951ce28f164a246ae933e8b6de6fa105e3aa52
Antonio Quartetto <antonioquartetto98@gmail.com>
2023-05-30 18:53:12 +0200

    creazione mvc e table categorie

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+/*-- Book Routes ---*/ 
 Route::get('/libri', [BookController::class, 'index'])->name('book.index');
 Route::get('/libri/crea', [BookController::class, 'create'])->name('book.create');
 Route::post('/libri/salva', [BookController::class, 'store'])->name('book.store');
 Route::get('/libri/{book}/dettagli', [BookController::class, 'show'])->name('book.show');
+
+
+/*-- Categories Routes --- */
+
+Route::get('/categorie', [BookController::class, 'index'])->name('category.index');
+Route::get('/categorie/crea', [BookController::class, 'create'])->name('category.create');
+Route::post('/categorie/salva', [BookController::class, 'store'])->name('category.store');
+Route::get('/categorie/{cat}/dettagli', [BookController::class, 'show'])->name('category.show');
+
