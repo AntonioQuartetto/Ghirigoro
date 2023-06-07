db631c7a419762f2c29aacc8580e5c19cd0b240a
Antonio Quartetto <antonioquartetto98@gmail.com>
2023-06-07 20:26:36 +0200

    aggiunti fillable nel model author

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'surname', 'birthday'];
+
+    protected $casts = [
+        'birthday' => 'datetime'
+    ];
+    
 }
