d4f0943d8ada0f10c2eb645f1a3922837e810f97
Antonio Quartetto <antonioquartetto98@gmail.com>
2023-06-07 22:07:00 +0200

    aggiunto show dettagli autore

---
+++ resources/views/authors/show.blade.php
@@ -0,0 +1,29 @@
+<x-main>
+
+<x-slot name="title">Dettagli Autore</x-slot>
+
+<div class="my-5 text-white">
+
+    <div class="text-center">
+      <h2 class="my-5">Dettagli Autore</h2>
+    </div>
+  
+  <div class="row">
+
+    <div class="col-12 col-sm-6">
+     
+    </div>
+    
+    <div class="col-12 col-sm-6">
+      <ul>
+      <li><b>Nome: </b>{{$author->name}}</li>
+      <li><b>Cognome: </b>{{$author->surname}} </li>
+      <li><b>Anno di Nascita: </b>{{$author->birthday}}</li>
+      </ul>
+    </div>
+
+
+
+  </div>
+
+</x-main>
