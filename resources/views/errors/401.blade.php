c7744a2700d1ec540b654a9f965b20ea22b1b617
Antonio Quartetto <antonioquartetto98@gmail.com>
2023-06-13 21:33:29 +0200

    modifica pagina 401

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,8 +1,17 @@
 
+<x-main>
 
+<x-slot name=title>401 Unauthorized</x-slot>
 
-<div class="container">
+    <div class="container text-center">
 
-    <img src="{{ Storage::url('\images\images-general\401-image.jpg') }}"
+        <img src="{{ Storage::url('\images\images-general\401-image.jpg') }}" class="unauthorized-custom">
 
-</div>
+         <p class="container mt-3">
+            <a href="{{ route('homepage') }}" class="btn-401-error py-2 px-4">
+                Torna alla Homepage
+            </a>
+        </p>
+    </div>
+
+</x-main>
